276d7d019cb8662fccd30dd2e027675b5fe6df55
johanswanepoel <johanswanepoel@jswanepo-osx.local>
2017-11-03 13:40:05 +1100

    Enabled debug in test

--- config/settings/test.py
+++ config/settings/test.py
@@ -10,7 +10,7 @@ from .base import *  # noqa
 # DEBUG
 # ------------------------------------------------------------------------------
 # Turn debug off so tests run faster
-DEBUG = False
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
 TEMPLATES[0]['OPTIONS']['debug'] = False
 
 # SECRET CONFIGURATION
