ad70691cc4406182d12da42ac9fb9ac1e117eee6
johanswanepoel <johannes.swanepoel@gmail.com>
2017-12-20 16:08:36 +1100

    Updates to support Django 2

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
+app_name="myaccounts"
+
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
--- myaccounts/accounts/urls.py
+++ myaccounts/accounts/urls.py
@@ -6,6 +6,8 @@ from django.views import defaults as default_views
 from . import views
 from . import views_api
 
+app_name="accounts"
+
 urlpatterns = [
     url(r'^$', views.AccountsMainView.as_view(), name='home'),
     url(r'^api/account_list/(?P<user_id>[0-9a-f-]+)/$', views_api.get_accounts, name='account_list_api')
