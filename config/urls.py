8d1878cb78c4819fde6593c7e98e11379804919e
Johan Swanepoel <johannes.swanepoel@gmail.com>
2020-04-10 17:55:56 +1000

    Simplified the dependencies

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from . import views
 
 app_name="myaccounts"
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    #url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', views.MainView.as_view(), name='home'),
+    
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
